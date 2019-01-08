f908c4e6bb883b0c3ea5d416f4fe5ce556f6494c
Matthew Foyle <matt@moltin.com>
2019-01-08 13:42:09 +0300

    Abstract the validation

--- src/main/kotlin/com/foyle/Controller.kt
+++ src/main/kotlin/com/foyle/Controller.kt
@@ -15,13 +15,13 @@ import com.foyle.models.NewAccount
 
 // Gson specific
 import com.google.gson.Gson
-import com.google.gson.JsonParser
 
 // Internal Service
 import com.foyle.internal.InternalServiceImpl
 
+import com.foyle.internal.Validation
+
 // Number specific
-import kotlin.math.sign
 import java.math.BigDecimal
 import java.math.RoundingMode
 
@@ -33,50 +33,6 @@ class Controller {
 
   init {
     initRoutes()
-  }
-
-  // Validates the body of a transfer request
-  private fun transferValidator(req: spark.Request) {
-    val jsonObj = JsonParser().parse(req.body()).getAsJsonObject()
-
-    if (!jsonObj.has("amount") || !jsonObj.has("recipient")) {
-      halt(400, gson.toJson("Check you have supplied a recipient and an amount in the body"))
-    }
-
-    if (jsonObj.get("amount").asDouble.sign != 1.0) {
-      halt(400, gson.toJson("You cannot transfer nothing or negative amounts"))
-    }
-  }
-
-  // Validates the content of a POST request
-  private fun postValidator(req: spark.Request) {
-
-    if (req.contentLength() == 0) {
-      halt(400, "Expected a payload with content in it")
-    }
-
-    if ("application/json" !in req.contentType()) {
-      halt(400, "Body must be of type application/json")
-    }
-  }
-
-  // Validates the specific body of a a new account request
-  private fun newAccountValidator(req: spark.Request) {
-    val jsonObj = JsonParser().parse(req.body()).getAsJsonObject()
-
-    if (!jsonObj.has("name") || !jsonObj.has("email")) {
-      halt(400, "You have not provided a name and email!")
-    }
-  }
-
-  // Validates an ID of an account being requested
-  private fun iDValidator(str: String) {
-    try {
-      str.toInt()
-    } catch(e: NumberFormatException) {
-      halt(400, "You have not provided a valid ID")
-    }
-
   }
 
   private fun initRoutes() {
@@ -108,7 +64,7 @@ class Controller {
       get("/:id", { req, res ->
 
         val id = req.params("id")
-        iDValidator(id)
+        Validation.iDValidator(id)
 
         try {
           res.status(200)
@@ -124,8 +80,8 @@ class Controller {
       // Create a new account
       post("/new", { req, res ->
 
-        postValidator(req)
-        newAccountValidator(req)
+        Validation.postValidator(req)
+        Validation.newAccountValidator(req)
 
         val payload: NewAccount = gson.fromJson(req.body(), NewAccount::class.java)
 
@@ -141,15 +97,15 @@ class Controller {
       // Transfer money from one account to another
       post("/:id/transfer", { req, res ->
 
-        postValidator(req)
-        transferValidator(req)
+        Validation.postValidator(req)
+        Validation.transferValidator(req)
 
         try {
 
           val payload: Transfer = gson.fromJson(req.body(), Transfer::class.java)
           val id = req.params("id")
 
-          iDValidator(id)
+          Validation.iDValidator(id)
 
           res.status(200)
           intService.transfer(id.toInt(), payload.recipient, BigDecimal(payload.amount).setScale(2, RoundingMode.DOWN))
---
+++ src/main/kotlin/com/foyle/internal/Validation.kt
@@ -0,0 +1,55 @@
+package com.foyle.internal
+
+import com.google.gson.JsonParser
+import com.google.gson.Gson
+import spark.Spark
+import kotlin.math.sign
+
+object Validation {
+
+    val gson = Gson()
+
+    // Validates the body of a transfer request
+    fun transferValidator(req: spark.Request) {
+        val jsonObj = JsonParser().parse(req.body()).getAsJsonObject()
+
+        if (!jsonObj.has("amount") || !jsonObj.has("recipient")) {
+            Spark.halt(400, gson.toJson("Check you have supplied a recipient and an amount in the body"))
+        }
+
+        if (jsonObj.get("amount").asDouble.sign != 1.0) {
+            Spark.halt(400, gson.toJson("You cannot transfer nothing or negative amounts"))
+        }
+    }
+
+    // Validates the content of a POST request
+    fun postValidator(req: spark.Request) {
+
+        if (req.contentLength() == 0) {
+            Spark.halt(400, "Expected a payload with content in it")
+        }
+
+        if ("application/json" !in req.contentType()) {
+            Spark.halt(400, "Body must be of type application/json")
+        }
+    }
+
+    // Validates the specific body of a a new account request
+    fun newAccountValidator(req: spark.Request) {
+        val jsonObj = JsonParser().parse(req.body()).getAsJsonObject()
+
+        if (!jsonObj.has("name") || !jsonObj.has("email")) {
+            Spark.halt(400, "You have not provided a name and email!")
+        }
+    }
+
+    // Validates an ID of an account being requested
+    fun iDValidator(str: String) {
+        try {
+            str.toInt()
+        } catch(e: NumberFormatException) {
+            Spark.halt(400, "You have not provided a valid ID")
+        }
+
+    }
+}
